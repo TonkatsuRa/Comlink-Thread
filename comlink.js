f53da42c7391e2ac82ba1f67eabc7eab4503e051
TonkatsuRa <115587388+TonkatsuRa@users.noreply.github.com>
2023-05-07 19:51:46 +0200

    Test comlink.js

--- comlink.js
+++ comlink.js
@@ -3,11 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const addUserBtn = document.querySelector('#add-user-btn');
   const deleteUserBtn = document.querySelector('#delete-user-btn');
   const userInputElement = document.querySelector('#alias-input');
-  const messageInputElement = document.querySelector('#message-input');
+  const messageInputElement = document.querySelector('[data-editor]');
   const alignmentSelect = document.querySelector('#alignment-select');
   const profilePicInput = document.querySelector('#profile-pic-input');
   const sendMessageBtn = document.querySelector('#send-message-btn');
-  const timestampInput = document.querySelector('#timestamp-input');
 
   let users = JSON.parse(localStorage.getItem('users')) || [{ name: 'Admin', profilePicture: '' }];
 
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
           const profilePicURL = reader.result;
           users.push({ name: userName, profilePicture: profilePicURL });
           updateUserList();
-          saveUsers(); // Save the users array to localStorage
+          saveUsers();
           userInputElement.value = '';
           profilePicInput.value = '';
         }
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
       } else {
         users.push({ name: userName, profilePicture: '' });
         updateUserList();
-        saveUsers(); // Save the users array to localStorage
+        saveUsers();
         userInputElement.value = '';
         profilePicInput.value = '';
       }
@@ -62,6 +61,10 @@ document.addEventListener('DOMContentLoaded', () => {
     return users.find((user) => user.name === name);
   }
 
+  function saveUsers() {
+    localStorage.setItem('users', JSON.stringify(users));
+  }
+
   sendMessageBtn.addEventListener('click', () => {
     const userName = aliases.value;
     const user = getUserByName(userName);
@@ -70,19 +73,17 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    const messageText = messageInputElement.value.trim().replace(/\n/g, '<br>');
+    const messageText = bbCodeToHTML(messageInputElement.value.trim());
     const alignment = alignmentSelect.value;
     const profilePicURL = user.profilePicture;
-    const timestampText = timestampInput.value;
 
     if (messageText) {
-      createMessage(userName, profilePicURL, messageText, alignment, timestampText);
+      createMessage(userName, profilePicURL, messageText, alignment);
       messageInputElement.value = '';
-      timestampInput.value = '';
     }
   });
 
-  function createMessage(name, profilePicURL, text, alignment, timestampText) {
+  function createMessage(userName, profilePicURL, messageText, alignment) {
     const message = document.createElement('div');
     message.classList.add('message', `message-${alignment}`);
 
@@ -101,105 +102,124 @@ document.addEventListener('DOMContentLoaded', () => {
     const moveUpBtn = document.createElement('button');
     moveUpBtn.classList.add('move-up-btn');
     moveUpBtn.textContent = '↑';
-    moveUpBtn.onclick = () => {
-      if (message.previousElementSibling) {
-        message.parentElement.insertBefore(message, message.previousElementSibling);
-      }
-    };
+      moveUpBtn.onclick = () => {
+        if (message.previousElementSibling) {
+          message.parentElement.insertBefore(message, message.previousElementSibling);
+        }
+      };
 
-    const moveDownBtn = document.createElement('button');
-    moveDownBtn.classList.add('move-down-btn');
-    moveDownBtn.textContent = '↓';
-    moveDownBtn.onclick = () => {
-      if (message.nextElementSibling) {
-        message.parentElement.insertBefore(message.nextElementSibling, message);
-      }
-    };
-
-    arrowContainer.appendChild(moveUpBtn);
-    arrowContainer.appendChild(moveDownBtn);
-
-    if (alignment === 'left') {
-      messageBox.appendChild(arrowContainer);
-      if (name !== 'Admin') {
-        messageBox.appendChild(profilePic);
-      }
+      const moveDownBtn = document.createElement('button');
+  moveDownBtn.classList.add('move-down-btn');
+  moveDownBtn.textContent = '↓';
+  moveDownBtn.onclick = () => {
+    if (message.nextElementSibling) {
+      message.parentElement.insertBefore(message.nextElementSibling, message);
     }
+  };
 
-    const messageContent = document.createElement('div');
-    messageContent.classList.add('message-content');
-    if (alignment === 'left') {
-      messageContent.setAttribute('data-augmented-ui', 'bl-clip tr-2-clip-x');
+  arrowContainer.appendChild(moveUpBtn);
+  arrowContainer.appendChild(moveDownBtn);
+
+  if (alignment === 'left') {
+    messageBox.appendChild(arrowContainer);
+    if (userName !== 'Admin') {
+      messageBox.appendChild(profilePic);
     }
-    if (alignment === 'right') {
-      messageContent.setAttribute('data-augmented-ui', 'br-clip tl-2-clip-x');
-    }
-
-    if (name !== 'Admin') {
-      const messageInfo = document.createElement('div');
-      messageInfo.classList.add('message-info');
-
-      const userName = document.createElement('span');
-      userName.classList.add('name');
-      userName.textContent = name;
-
-      const timestamp = document.createElement('span');
-      timestamp.classList.add('timestamp');
-      timestamp.textContent = timestampText || new Date().toLocaleTimeString();
-
-      const messageText = document.createElement('p');
-      messageText.innerHTML = text;
-
-      messageInfo.appendChild(userName);
-      messageInfo.appendChild(timestamp);
-
-      messageContent.appendChild(messageInfo);
-      messageContent.appendChild(messageText);
-    } else {
-      messageContent.style.background = 'transparent';
-      messageContent.style.border = 'none';
-      messageContent.style.boxShadow = 'none';
-      messageContent.style.outline = 'none';
-      messageContent.style.borderColor = 'transparent';
-      messageContent.style.color = '#000'; /* Reset text color to black */
-      messageContent.setAttribute('data-augmented-ui', 'none');
-      messageContent.innerHTML = text;
-      message.classList.add('admin-message');
-      messageContent.classList.add('admin-message-content');
-    }
-
-    messageBox.appendChild(messageContent);
-
-    if (alignment === 'right') {
-      if (name !== 'Admin') {
-        messageBox.appendChild(profilePic);
-      }
-      messageBox.appendChild(arrowContainer);
-    }
-
-    message.appendChild(messageBox);
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.textContent = 'x';
-    deleteBtn.onclick = () => {
-      message.remove();
-    };
-
-    messageBox.appendChild(deleteBtn);
-
-    document.querySelector('.comlink-container').appendChild(message);
   }
 
-  // Save Users Function //
-  function saveUsers() {
-    localStorage.setItem('users', JSON.stringify(users));
+  const messageContent = document.createElement('div');
+  messageContent.classList.add('message-content');
+  if (alignment === 'left') {
+    messageContent.setAttribute('data-augmented-ui', 'bl-clip tr-2-clip-x');
+  }
+  if (alignment === 'right') {
+    messageContent.setAttribute('data-augmented-ui', 'br-clip tl-2-clip-x');
+  }
+
+  if (userName !== 'Admin') {
+    const messageInfo = document.createElement('div');
+    messageInfo.classList.add('message-info');
+
+    const userNameSpan = document.createElement('span');
+    userNameSpan.classList.add('name');
+    userNameSpan.textContent = userName;
+
+    const timestampSpan = document.createElement('span');
+    timestampSpan.classList.add('timestamp');
+    timestampSpan.textContent = new Date().toLocaleTimeString();
+
+    const messageTextP = document.createElement('p');
+    messageTextP.innerHTML = messageText;
+
+    messageInfo.appendChild(userNameSpan);
+    messageInfo.appendChild(timestampSpan);
+
+    messageContent.appendChild(messageInfo);
+    messageContent.appendChild(messageTextP);
+  } else {
+    messageContent.style.background = 'transparent';
+    messageContent.style.border = 'none';
+    messageContent.style.boxShadow = 'none';
+    messageContent.style.outline = 'none';
+    messageContent.style.borderColor = 'transparent';
+    messageContent.style.color = '#000';
+    messageContent.setAttribute('data-augmented-ui', 'none');
+    messageContent.innerHTML = messageText;
+    message.classList.add('admin-message');
+    messageContent.classList.add('admin-message-content');
+  }
+
+  messageBox.appendChild(messageContent);
+
+  if (alignment === 'right') {
+    if (userName !== 'Admin') {
+      messageBox.appendChild(profilePic);
+    }
+    messageBox.appendChild(arrowContainer);
+  }
+
+  message.appendChild(messageBox);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.textContent = 'x';
+  deleteBtn.onclick = () => {
+    message.remove();
+  };
+
+  messageBox.appendChild(deleteBtn);
+
+  document.querySelector('.comlink-container').appendChild(message);
+}
+
+function bbCodeToHTML(text) {
+  const bbCodes = [
+    { regex: /\[b\](.*?)\[\/b\]/g, replacement: '<strong>$1</strong>' },
+    { regex: /\[i\](.*?)\[\/i\]/g, replacement: '<em>$1</em>' },
+    { regex: /\[u\](.*?)\[\/u\]/g, replacement: '<u>$1</u>' },
+    { regex: /\[s\](.*?)\[\/s\]/g, replacement: '<strike>$1</strike>' },
+    { regex: /\[left\](.*?)\[\/left\]/g, replacement: '<div style="text-align: left">$1</div>' },
+    { regex: /\[center\](.*?)\[\/center\]/g, replacement: '<div style="text-align: center">$1</div>' },
+    { regex: /\[right\](.*?)\[\/right\]/g, replacement: '<div style="text-align: right">$1</div>' },
+  { regex: /\[color=(.*?)\](.*?)\[\/color\]/g, replacement: '<span style="color:$1">$2</span>' },
+  { regex: /\[size=(.*?)\](.*?)\[\/size\]/g, replacement: '<span style="font-size:$1px">$2</span>' },
+  { regex: /\[url\](.*?)\[\/url\]/g, replacement: '<a href="$1" target="_blank">$1</a>' },
+  { regex: /\[url=(.*?)\](.*?)\[\/url\]/g, replacement: '<a href="$1" target="_blank">$2</a>' },
+  { regex: /\[img\](.*?)\[\/img\]/g, replacement: '<img src="$1" alt="Image" />' },
+  { regex: /\[quote\](.*?)\[\/quote\]/g, replacement: '<blockquote>$1</blockquote>' },
+  { regex: /\[list\](.*?)\[\/list\]/g, replacement: '<ul>$1</ul>' },
+  { regex: /\[\*\](.*?)\[\/\*\]/g, replacement: '<li>$1</li>' },
+];
+
+    return bbCodes.reduce((acc, bbCode) => {
+      return acc.replace(bbCode.regex, bbCode.replacement);
+    }, text);
   }
 
   function loadUsers() {
     updateUserList();
   }
 
-  // ====== END OF THE SCRIPT ===== //
   loadUsers();
 });
+
